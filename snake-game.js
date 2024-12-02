1bd946bee9ceafb8c53b2dbaab463cf2cf17dd8c
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-12-02 07:41:16 +0100

    Update snake-game.js

--- snake-game.js
+++ snake-game.js
@@ -1,21 +1,30 @@
+
+
 // Initialize the game based on mode
 function initGame(mode, canvas1Id, canvas2Id = null) {
     let game;
-    switch(mode) {
+
+    switch (mode) {
         case 'basic':
             game = new BasicGame(canvas1Id);
             break;
         case 'powerup':
-            game = new PowerUpGame(canvas1Id);
-            break;
+                game = new PowerUpGame(canvas1Id);
+                break;
         case 'twoplayer':
             game = new TwoPlayerGame(canvas1Id, canvas2Id);
             break;
+        // Ha a jövőben bővítenéd további játékmódokkal:
+        // case 'advanced':
+        //     game = new AdvancedGame(canvas1Id);
+        //     break;
+
         default:
             throw new Error('Invalid game mode');
     }
 
-    const gameLoop = setInterval(() => {
+    // Létrehozunk egy játékciklust a példányra, hogy egyedi időzítőt tudjunk kezelni
+    game.gameLoop = setInterval(() => {
         game.update();
         game.draw();
     }, GAME_SPEED);
