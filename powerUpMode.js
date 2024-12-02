b1767b68f53cc2bc1c4144c14df0463498808841
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-12-02 07:41:00 +0100

    Update powerUpMode.js

--- powerUpMode.js
+++ powerUpMode.js
@@ -1,14 +1,44 @@
-
-
 // powerUpMode.js - Game mode with special abilities
 class PowerUpGame extends BasicGame {
     constructor(canvasId) {
         super(canvasId);
         this.powerUps = [];
         this.powerUpTypes = {
-            speed: { color: '#FFD700', duration: 5000, effect: () => this.setGameSpeed(50) },
-            ghost: { color: '#87CEEB', duration: 3000, effect: () => this.snake.abilities.add('ghost') },
-            double: { color: '#FF69B4', duration: 4000, effect: () => this.snake.growing = true }
+            speed: { 
+                color: '#FFD700', 
+                duration: 5000, 
+                effect: () => this.setGameSpeed(50) 
+            },
+            ghost: { 
+                color: '#87CEEB', 
+                duration: 3000, 
+                effect: () => this.snake.abilities.add('ghost') 
+            },
+            double: { 
+                color: '#FF69B4', 
+                duration: 4000, 
+                effect: () => this.snake.growing = true 
+            },
+            slow: { 
+                color: '#8A2BE2', 
+                duration: 5000, 
+                effect: () => this.setGameSpeed(200) 
+            },
+            wallPass: { 
+                color: '#00FA9A', 
+                duration: 5000, 
+                effect: () => this.snake.abilities.add('wallPass') 
+            },
+            freeze: { 
+                color: '#DC143C', 
+                duration: 3000, 
+                effect: () => this.freezeGame() 
+            },
+            doublePoints: { 
+                color: '#FFDAB9', 
+                duration: 10000, 
+                effect: () => this.doublePointsMode() 
+            }
         };
     }
 
@@ -29,7 +59,7 @@ class PowerUpGame extends BasicGame {
             this.update();
             this.draw();
         }, speed);
-        
+
         setTimeout(() => {
             clearInterval(this.gameLoop);
             this.gameLoop = setInterval(() => {
@@ -37,6 +67,23 @@ class PowerUpGame extends BasicGame {
                 this.draw();
             }, GAME_SPEED);
         }, this.powerUpTypes.speed.duration);
+    }
+
+    freezeGame() {
+        clearInterval(this.gameLoop);
+        setTimeout(() => {
+            this.gameLoop = setInterval(() => {
+                this.update();
+                this.draw();
+            }, GAME_SPEED);
+        }, 3000); // 3 seconds freeze
+    }
+
+    doublePointsMode() {
+        this.snake.abilities.add('doublePoints');
+        setTimeout(() => {
+            this.snake.abilities.delete('doublePoints');
+        }, this.powerUpTypes.doublePoints.duration);
     }
 
     update() {
@@ -61,11 +108,23 @@ class PowerUpGame extends BasicGame {
             }
             return true;
         });
+
+        // Handle wall passing
+        if (this.snake.abilities.has('wallPass')) {
+            if (head.x < 0) head.x = GRID_SIZE - 1;
+            else if (head.x >= GRID_SIZE) head.x = 0;
+            if (head.y < 0) head.y = GRID_SIZE - 1;
+            else if (head.y >= GRID_SIZE) head.y = 0;
+        } else {
+            if (head.x < 0 || head.x >= GRID_SIZE || head.y < 0 || head.y >= GRID_SIZE) {
+                this.gameOver = true;
+            }
+        }
     }
 
     draw() {
         super.draw();
-        
+
         // Draw power-ups
         this.powerUps.forEach(powerUp => {
             this.ctx.fillStyle = powerUp.color;
